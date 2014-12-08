c39310b635fdb992c3a7698bcb5f93d9dd464a1e
wzj <wangzhaojiang2013@gmail.com>
2014-12-09 01:09:27 +0800

    改进 function query()

--- source/inc/conn.php
+++ source/inc/conn.php
@@ -1,20 +1,16 @@
 <?php
 class Nlpdb{
-	private $hostname;
-	private $dbuser;
-	private $dbpwd;
-	private $dbname;
-	private $charName;
+	
 	private $db;
 
-	private $res;
+	private $result;
 	
 	function __construct()
 	{
-		$hostname = "";
-		$dbuser = "";
-		$dbpwd = "";
-		$dbname = "";
+		$hostname = "localhost";
+		$dbuser = "root";
+		$dbpwd = "notamaiba";
+		$dbname = "jaker_me";
 		$charName = "utf8";
 
 		$this->db = new mysqli($hostname, $dbuser, $dbpwd, $dbname);
@@ -24,21 +20,33 @@ class Nlpdb{
 			echo "链接失败".mysqli_connect_errno();
 			exit();
 		}
-		$this->db->query("set name $charName;");
+		$this->db->query("set names $charName;");
 
-		return $this->db;
 	}
 
-	public function query($sql)
+	public function query($sql_str, $sql_error = 'error')
 	{
-		$this->res = $this->db->query($sql);
-		return $this->res;
+        var_dump($sql_str);
+        $this->result = $this->db->query($sql_str);
+        if ($this->result == false){
+            echo $sql_error;
+            return $this->result;
+        }
+
+        if (is_object($this->result) and $this->result -> num_rows > 0){
+            while( $row = $this->result -> fetch_assoc() )
+                $array[] = $row;
+            return $array;
+        }
+
+        return $this->result;
+
 	}
 
 	function __destruct()
 	{
-		if( is_object($this->res) )
-			$this->res->free();
+		if( is_object($this->result) )
+			$this->result->free();
 		$this->db->close();
 	}
 }
