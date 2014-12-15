c2b8028e757fde7a09454bc883edbc1fea75096c
wzj <wangzhaojiang2013@gmail.com>
2014-12-16 00:48:53 +0800

    rebuild relation.class.php

--- source/inc/relation.class.php
+++ source/inc/relation.class.php
@@ -4,9 +4,11 @@
 class Relation{
 	
 	private $conn;
+    private $fromuid;
 
-	public function __construct(){
+	public function __construct($id){
 		$this->conn = new Nlpdb();
+        $this->fromuid = $id;
 	}
 
 	/*@add_relation:添加关注着信息
@@ -14,36 +16,55 @@ class Relation{
 	 *@to_uid，是被关注者
 	 *return:返回false/true
 	 */
-	public function add_relation($from_uid, $to_uid){
-		if (!is_numeric($from_uid) || !is_numeric($to_uid) || ($from_uid == $to_uid)) {
-			return false;
-		}
+	public function add_relation($touid){
+		#if (!is_numeric($from_uid) || !is_numeric($to_uid) || ($from_uid == $to_uid)) {
+		#	return false;
+		#}
+        $query_str = "select fromuid, touid from user_relation where fromuid = $this->fromuid and touid = $touid";
+
+        $result = $this->conn->query($query_str);
+
+        if ($result != 'nothing'){
+            return array("result" => false, "error" => "can't follow a man again");
+        }
+
+		$query_str = "insert into `user_relation`(fromuid, touid) value('$this->fromuid', '$touid')";
 		
-		$query_str = "insert into `user_relation`(fromuid, touid) value('$from_uid', '$to_uid')";
-		
-		$sql_error = "insert into sql error";
+		$sql_error = "add_relation error";
 		
 		$result = $this->conn->query($query_str, $sql_error);
+
+        if ($result == false){
+            return array("result" => false, "error" => "follow error");
+        }
+        else{
+            return array("result" => true);   
+        }
 		
-		return $result;
 	}
 	/*@delete_relation:取消关注
 	 *@from_uid:是关注者
 	 *@to_uid:是被关注者
 	 *return:返回false/ture
 	 */
-	public function delete_relation($from_uid, $to_uid) {
-		if (!is_numeric($from_uid) || !is_numeric($to_uid) || ($from_uid == $to_uid)) {
-			return false;
-		}
+	public function delete_relation($touid) {
+		#if (!is_numeric($from_uid) || !is_numeric($to_uid) || ($from_uid == $to_uid)) {
+		#	return false;
+		#}
 		
-		$query_str = "delete from  `user_relation` where fromuid = '$from_uid' and touid = '$to_uid'";
+		$query_str = "delete from  `user_relation` where fromuid = '$this->fromuid' and touid = '$touid'";
 		
-		$sql_error = "delete  sql error";
+		$sql_error = "delete_relation  sql error";
 		
 		$result = $this->conn->query($query_str);
 
-		return $result;
+        if ($result == false){
+            return array("result" => false, "error" => "cancel follow error");
+        }
+        else{
+            return array("result" => true);
+        }
+
 	}
 	
 	/*@cat_relation:查看关注uid的人和查看uid关注的人
@@ -51,38 +72,50 @@ class Relation{
 	 *@tag：tag = 0, 查询uid关注的人，tag=1,查询关注uid的人
 	 *return:返回用户的头像，用户名，个性签名和学校的二维数组,没有数据返回false
 	 */
-	public function cat_relation($uid, $tag) { 
-		if (!is_numeric($uid)&& ($tag != 0 || $tag != 1))  {
-			return false;
+	public function cat_relation($tag) { 
+		if (($tag != 0 && $tag != 1))  {
+			return array("result" => false, "error" => "tag error");
 		}
 		
 		if ($tag == 0) {
-			$query_str = "select distinct(name), school, logo, sign  from `user_info`  where user_info.uid = any(select touid from `user_relation` where fromuid = '$uid' )";
+			$query_str = "select uid, name, logo, sign from `user_info`  where user_info.uid = any(select touid from `user_relation` where fromuid = '$this->fromuid' )";
 
-			$sql_error = "select cat_relation sql error";
+			$sql_error = "cat_relation sql error";
+
 			$result = $this->conn->query($query_str, $sql_error);
 
 			if ($result == false) {
 				
-				return false;
+				return array("result" => false, "error" => "cat follow error");
 			}
+            else if ($result == 'nothing'){
+                return array("result" => true, "content" => null);
+            }
+            else{
+			    return array("result" => true, "content" => $result);
+            
+            }
 			
-			return $result;
 			
 		}
 
 		if ($tag  == 1) {
-			$query_str = "select name, school, logo, sign from `user_info`where user_info.uid = any(select fromuid from `user_relation` where touid = '$uid')";
+			$query_str = "select uid, name, logo, sign from `user_info`where user_info.uid = any(select fromuid from `user_relation` where touid = '$this->fromuid')";
 			
-			$sql_error = "select cat_relation sql error";
+			$sql_error = "cat_relation sql error";
 			$result = $this->conn->query($query_str, $sql_error);
 
 			if ($result == false) {
 				
-				return false;
+				return array("result" => false, "error" => "cat fans error");
 			}
-			
-			return $result;
+            else if ($result == 'nothing'){
+                return array("result" => true, "content" => null);
+            }
+            else{
+                return array("result" => true, "content" => $result);
+            }
+
 		}
 	}
 
@@ -92,34 +125,45 @@ class Relation{
 	 *@tag：tag=0,查询uid关注的人的数量，tag=1,查询关注uid的人的数量
 	 *return:返回关注者的数量int,出错返回false
 	 */
-	public function count_relation($uid, $tag) {
-		if (!is_numeric($uid) &&($tag != 0 || $tag != 1))  {
-			return false;
+	public function count_relation($tag) {
+		if (($tag != 0 && $tag != 1))  {
+			return array("result" => false, "error" => "tag error");
 		}
 		
 		if ($tag == 0) {
-			$query_str = "select count(uid) from `user_info`  where user_info.uid = any(select touid from `user_relation` where fromuid = '$uid' )";
+			$query_str = "select count(uid) from `user_info`  where user_info.uid = any(select touid from `user_relation` where fromuid = '$this->fromuid' )";
 			
-			$sql_error = "select count_relation sql error";
+			$sql_error = "count_relation sql error";
 			$result = $this->conn->query($query_str, $sql_error);
 
 			if ($result == false) {
-				return false;
+				return array("result" => false, "error" => "count follow error");
 			}
-			return (int)$result[0]["count(uid)"];
+            else if ($result == 'nothing'){
+                return array("result" => true, "content" => 0);
+            }
+            else{
+			    return array("result" => true, "content" => (int)$result[0]["count(uid)"]);
+            }
 		}
 
 		if ($tag  == 1) {
-			$query_str = "select count(uid) from `user_info`where user_info.uid = any(select fromuid from `user_relation` where touid = '$uid')";
+			$query_str = "select count(uid) from `user_info`where user_info.uid = any(select fromuid from `user_relation` where touid = '$this->fromuid')";
 			
-			$sql_error = "select count_relation sql error";
+			$sql_error = "count_relation sql error";
 			$result = $this->conn->query($query_str, $sql_error);
 
 			if ($result == false) {
-				return false;
+				return array("result" => false, "error" => "count fans error");
 			}
+            else if ($result == 'nothing'){
+                return array("result" => true, "content" => 0);
+            }
+            else{
+			    return array("result" => true, "content" => (int)$result[0]["count(uid)"]);
+            
+            }
 			
-			return (int)$result[0]["count(uid)"];
 		}
 		
 	}
