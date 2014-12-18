0ceff0f66ab49711064a8eaa63007b79af248538
Jensyn <zhangyongjun369@gmail.com>
2014-12-18 15:01:51 +0800

    更新前端模板

--- source/js/app_init.js
+++ source/js/app_init.js
@@ -1,20 +1,33 @@
+var FormUtil = {
+    checkEmail: function(str) {
+        var pattern = /^.*@.*\..*$/;
+        return pattern.test(str);
+    }, 
+    checkPhone: function(str) {
+    	var pattern = /^\d{11}$/;
+    	return pattern.test(str); 
+    },
+    checkPwd: function(str) {
+        var pattern = /^[^\s.]*$/;
+        return pattern.test(str);
+    }
+};
+
 $(document).ready(function() {
         // 设置page-wrapper高度
-    var height = $(window).height() - $('footer').height() - $('header').height() - 10;
-    $('.page-wrapper').css('min-height', height);
+	function resizePageWrapper() {
+    	var pageHeight = $(window).height() - $('footer').height() - $('header').height() - 10;
+    	$('.page-wrapper').css('min-height', pageHeight);
+	}
 
-    $('.navigation .menu .fa-drop-down').click(function(){
-        var status= $('.navigation .menu .course-drop-down').css('display');
-        if (status == 'block') {
-            $('.navigation .menu .course-drop-down').css('display', 'none');
-        } else {
-            $('.navigation .menu .course-drop-down').css('display', 'block');
-        }
+    $('.navigation .menu-course').mouseenter(function(){
+        var status= $('.navigation .course-drop-down-modal').css('display');
+        $('.navigation .course-drop-down-modal').css('display', 'block');
     });
-/*    $('.navigation .menu .course-drop-down').click(function(){
-        $('.menu .drop-down').css('display', 'none');
+    $('.navigation .course-drop-down-modal').mouseleave(function() {
+        $('.navigation .course-drop-down-modal').css('display', 'none');    	
     });
-*/
+    
     $('.navigation-right .link-profile').click(function() {
         var status= $('.navigation-right .drop-down-list').css('display');
         if (status == 'block') {
@@ -24,7 +37,12 @@ $(document).ready(function() {
         }
     });
     
-	$('a.btn').click(function(){
-		return false;
+//	$('a.btn').click(function(){
+//		return false;
+//	});
+	
+	resizePageWrapper();
+	$(window).on('resize', function(){
+		resizePageWrapper();	
 	});
 });
