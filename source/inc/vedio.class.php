1f4e5ed9e64dbc5280db44bb70f930a7854c4e42
wzj <wangzhaojiang2013@gmail.com>
2015-01-23 12:47:40 +0800

    update

--- source/inc/vedio.class.php
+++ source/inc/vedio.class.php
@@ -12,7 +12,38 @@ class Vedio{
 
     #观看视频
     public function watch_vedio($vid, $fromuid = 0){
-        $query_str = "select  ";
+        $query_str = "select vid, uid, vedio_name, ,vedio_url, vedio_tag, vedio_photo, vedio_sort, vedio_watch, time from vedio_info where vid = $vid";
+        
+        $sql_error = "watch_vedio error";
+
+        $result = $this->conn->query($query_str, $sql_error);
+
+        if ($result == false){
+            return array("status" => false, "error" => "watch vedio error");
+        }
+        else if ($result == 'nothing'){
+            return array("status" => true, "content" => null);
+        }
+        else{
+            
+            if ($fromuid != 0){
+                #观看数增加
+                $query_str = "update vedio_info set vedio_watch = vedio_watch + 1 where vid = $vid";
+
+                $this->conn->query($query_str);
+
+                #观看记录增加
+
+                $time = date("Y-m-d H:i:s", time());
+
+                $query_str = "insert into plan(uid, vid, time, flag) values($fromuid, $vid, $time, 0)";
+
+                $this->conn->query($query_str);
+            }
+            return array("status" => true, "content" => $result);
+
+        }
+
     }
     
     #展示目标用户所有视频页面
