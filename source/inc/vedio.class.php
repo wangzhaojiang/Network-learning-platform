c7f63749ba55950a69019e115ed6c57a6354039a
wzj <wangzhaojiang2013@gmail.com>
2015-02-09 21:05:55 +0800

    vedio watch and cat inc

--- source/inc/vedio.class.php
+++ source/inc/vedio.class.php
@@ -5,14 +5,13 @@ class Vedio{
     private $conn;
     private $touid;
 
-    public function __construct($fromuid, $touid){
+    public function __construct(){
         $this->conn = new Nlpdb();
-        $this->touid = $touid; #目标用户ID
     }
 
     #观看视频
     public function watch_vedio($vid, $fromuid = 0){
-        $query_str = "select vid, uid, vedio_name, ,vedio_url, vedio_tag, vedio_photo, vedio_sort, vedio_watch, time from vedio_info where vid = $vid";
+        $query_str = "select vedio_url from vedio_info where vid = $vid";
         
         $sql_error = "watch_vedio error";
 
@@ -25,18 +24,18 @@ class Vedio{
             return array("status" => true, "content" => null);
         }
         else{
+            #观看数增加
+            $query_str = "update vedio_info set vedio_watch = vedio_watch + 1 where vid = $vid";
+
+            $this->conn->query($query_str);
             
             if ($fromuid != 0){
-                #观看数增加
-                $query_str = "update vedio_info set vedio_watch = vedio_watch + 1 where vid = $vid";
-
-                $this->conn->query($query_str);
 
                 #观看记录增加
 
                 $time = date("Y-m-d H:i:s", time());
 
-                $query_str = "insert into plan(uid, vid, time, flag) values($fromuid, $vid, $time, 0)";
+                $query_str = "insert into plan(uid, vid, time, flag) values($fromuid, $vid, '$time', 0)";
 
                 $this->conn->query($query_str);
             }
@@ -48,7 +47,7 @@ class Vedio{
     
     #展示目标用户所有视频页面
     public function cat_vedio(){
-        $query_str = "select vid, uid, vedio_name, vedio_tag, vedio_photo, vedio_sort, vedio_watch, time from vedio_info where uid = $this->touid and vedio_status = 1";
+        $query_str = "select vid, uid, vedio_name, vedio_tag, vedio_photo, vedio_sort, vedio_watch, time from vedio_info where vedio_status = 1";
 
         $sql_error = "cat_vedio select error";
 
